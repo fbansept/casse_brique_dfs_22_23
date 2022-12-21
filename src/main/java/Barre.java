c84e36dffdb19be7dd3a8389fc1f393db38c7937
fbansept <bansept.franck@gmail.com>
2022-12-21 15:25:15 +0100

    Ajout de sprite

---
+++ src/main/java/Barre.java
@@ -0,0 +1,23 @@
+public class Barre extends Sprite {
+
+    private int largeur = 200;
+    private int hauteur = 30;
+
+
+
+    public int getLargeur() {
+        return largeur;
+    }
+
+    public void setLargeur(int largeur) {
+        this.largeur = largeur;
+    }
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    public void setHauteur(int hauteur) {
+        this.hauteur = hauteur;
+    }
+}
